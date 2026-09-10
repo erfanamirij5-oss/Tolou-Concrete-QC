160171203cfd33acdabe035516240f6bcb100922
erfanamiriyazd <erfanamirij5@gmail.com>
2026-09-10 09:54:56 +0330

    Wire QC parties service into desktop runtime

--- src/main/database.ts
+++ src/main/database.ts
@@ -5,6 +5,7 @@ import { migrate } from '../infrastructure/sqlite/migrate.js';
 import { createLaboratoryService } from '../application/laboratory.js';
 import { createProjectService } from '../application/projects.js';
 import { createEngineeringService } from '../application/engineering.js';
+import { createQcPartiesService } from '../application/qc-parties.js';
 
 const COMPANY_ID = 'tolou-local-company';
 const DEFAULT_COMPANY_NAME = 'شرکت شما';
@@ -13,12 +14,14 @@ const LOCAL_ACTOR = 'کاربر محلی';
 export type LaboratoryService = ReturnType<typeof createLaboratoryService>;
 export type ProjectService = ReturnType<typeof createProjectService>;
 export type EngineeringService = ReturnType<typeof createEngineeringService>;
+export type QcPartiesService = ReturnType<typeof createQcPartiesService>;
 
 export interface DatabaseRuntime {
   db: DatabaseSync;
   laboratory: LaboratoryService;
   projects: ProjectService;
   engineering: EngineeringService;
+  qcParties: QcPartiesService;
 }
 
 export function createDatabaseRuntime(): DatabaseRuntime {
@@ -32,5 +35,6 @@ export function createDatabaseRuntime(): DatabaseRuntime {
     laboratory: createLaboratoryService(db, {companyId: COMPANY_ID, actor: LOCAL_ACTOR}),
     projects: createProjectService(db, {companyId: COMPANY_ID}),
     engineering: createEngineeringService(db, {companyId: COMPANY_ID, actor: LOCAL_ACTOR}),
+    qcParties: createQcPartiesService(db, {companyId: COMPANY_ID, actor: LOCAL_ACTOR}),
   };
 }
