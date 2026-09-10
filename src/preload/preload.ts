02dd5367e87ef66c3bd051bd2ba3df7ac39df26a
erfanamiriyazd <erfanamirij5@gmail.com>
2026-09-10 08:30:40 +0330

    Expose laboratory IPC through secure preload bridge

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,9 +1,12 @@
 import { contextBridge, ipcRenderer } from 'electron';
-import { IPC_CHANNELS, type TolouBridge } from '../shared/ipc.js';
+import { IPC_CHANNELS, type CreateSeriesInput, type LaboratoryKind, type SaveDraftInput, type TolouBridge } from '../shared/ipc.js';
 
 const bridge: TolouBridge = Object.freeze({
   getAppInfo: () => ipcRenderer.invoke(IPC_CHANNELS.appInfo),
   health: () => ipcRenderer.invoke(IPC_CHANNELS.health),
+  createSeries: (input: CreateSeriesInput) => ipcRenderer.invoke(IPC_CHANNELS.createSeries, input),
+  listSeries: (kind: LaboratoryKind, projectId?: string) => ipcRenderer.invoke(IPC_CHANNELS.listSeries, { kind, projectId }),
+  saveDraft: (input: SaveDraftInput) => ipcRenderer.invoke(IPC_CHANNELS.saveDraft, input),
 });
 
 contextBridge.exposeInMainWorld('tolou', bridge);
