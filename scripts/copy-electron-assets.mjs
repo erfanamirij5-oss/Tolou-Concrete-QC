29238143e9206d9c532ed38de392403216532942
erfanamiriyazd <erfanamirij5@gmail.com>
2026-09-11 17:36:33 +0330

    chore(build): package rule evaluation integrity migration

--- scripts/copy-electron-assets.mjs
+++ scripts/copy-electron-assets.mjs
@@ -21,6 +21,7 @@ const assets = [
   ['src/infrastructure/sqlite/017-qc-exceptions.sql', 'dist-electron/infrastructure/sqlite/017-qc-exceptions.sql'],
   ['src/infrastructure/sqlite/018-qc-exception-integrity.sql', 'dist-electron/infrastructure/sqlite/018-qc-exception-integrity.sql'],
   ['src/infrastructure/sqlite/019-qc-exception-current-view.sql', 'dist-electron/infrastructure/sqlite/019-qc-exception-current-view.sql'],
+  ['src/infrastructure/sqlite/020-rule-evaluation-integrity.sql', 'dist-electron/infrastructure/sqlite/020-rule-evaluation-integrity.sql'],
   ['assets/icons/Tolou-Concrete-QC.ico', 'dist-electron/assets/icons/Tolou-Concrete-QC.ico'],
 ];
 
